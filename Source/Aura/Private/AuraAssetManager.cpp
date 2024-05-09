30dcb8dbce2c86cb020a857f5f88745691b1d4cc
TheCircus <jonathanmann88@gmail.com>
2024-05-09 15:05:25 +0200

    Receiving Target Data

--- Source/Aura/Private/AuraAssetManager.cpp
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -15,6 +15,6 @@ UAuraAssetManager& UAuraAssetManager::Get()
 void UAuraAssetManager::StartInitialLoading()
 {
 	Super::StartInitialLoading();
-
 	FAuraGameplayTags::InitializeNativeGameplayTags();
+	//In lecture 117 it says to use UAbilitySystemGlobals::InitGlobalData but is not necessary in 5.3
 }
