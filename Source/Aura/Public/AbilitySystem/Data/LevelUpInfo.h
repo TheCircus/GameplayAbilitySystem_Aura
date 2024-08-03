85a25018379cf3c12aac26a7746907d7a612be49
TheCircus <jonathanmann88@gmail.com>
2024-08-03 11:47:18 +0200

    Level Up Info Data Asset

---
+++ Source/Aura/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,39 @@
+// Copyright JonBoy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraLevelUpInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 LevelUpRequirement = 0;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 AttributePointAward = 0;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpellPointAward = 0;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FAuraLevelUpInfo> LevelUpInformation;
+
+	int32 FindLevelForXP(int32 XP);
+	
+	
+};
