7b42b9467b81231081b196c9ba2b67d9f4f0a0c8
TheCircus <jonathanmann88@gmail.com>
2024-08-27 14:47:26 +0200

    Spell Menu Buttons

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -221,6 +221,11 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	FName("Abilities.Attack"),
 	FString("Attack Ability Tag")
 	);
+			
+	GameplayTags.Abilities_None = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Abilities.None"),
+	FString("No Ability - Like the nullptr for Ability Tags")
+	);
 		
 	GameplayTags.Abilities_Summon = UGameplayTagsManager::Get().AddNativeGameplayTag(
 	FName("Abilities.Summon"),
