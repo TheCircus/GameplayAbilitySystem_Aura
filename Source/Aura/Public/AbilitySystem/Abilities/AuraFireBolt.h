afaf144bb7ea6725a28790d76d028e8d51bfce33
TheCircus <jonathanmann88@gmail.com>
2024-09-07 16:55:09 +0200

    FireBolt Projectile Spread

--- Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
@@ -15,5 +15,16 @@ class AURA_API UAuraFireBolt : public UAuraProjectileSpell
 	GENERATED_BODY()
 public:
 	virtual FString GetDescription(int32 Level) override;
-	virtual FString GetNextLevelDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level)override;
+
+	UFUNCTION(BlueprintCallable)
+	void SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTarget);
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Fire")
+	float ProjectileSpread = 90.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Fire")
+	float MaxNumProjectiles = 5;
 };
