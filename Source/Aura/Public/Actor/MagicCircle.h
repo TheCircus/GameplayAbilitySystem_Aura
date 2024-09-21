593a95d32fb2ba9014e3bdcbe15f2ef29583ec93
TheCircus <jonathanmann88@gmail.com>
2024-09-21 19:21:56 +0200

    Magic Circle Interface Functions

--- Source/Aura/Public/Actor/MagicCircle.h
+++ Source/Aura/Public/Actor/MagicCircle.h
@@ -15,10 +15,11 @@ public:
 	AMagicCircle();
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+	
 protected:
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	TObjectPtr<UDecalComponent> MagicCircleDecal;
 
 };
