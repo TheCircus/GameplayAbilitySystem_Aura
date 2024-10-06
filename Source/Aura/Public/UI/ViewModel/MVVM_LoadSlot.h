483fbe0cee655629a51be51117ff50e4aa8e3c32
TheCircus <jonathanmann88@gmail.com>
2024-10-06 16:22:43 +0200

    Binding Variables to ViewModels

--- Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
@@ -21,14 +21,20 @@ public:
 	FSetWidgetSwitcherIndex SetWidgetSwitcherIndex;
 
 	void InitializeSlot();
-
-	UPROPERTY()
-	FString PlayerName;
 	
 	UPROPERTY()
 	FString LoadSlotName;
 	
 	UPROPERTY()
 	FString SlotIndex;
+
+	/** Field Notifies **/
+
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, FieldNotify, Setter, Getter)
+	FString PlayerName;
+
+	void SetPlayerName(FString InPlayerName);
+
+	FString GetPlayerName() const { return PlayerName; }
 };
